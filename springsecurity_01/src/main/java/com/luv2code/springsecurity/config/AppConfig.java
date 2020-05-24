5d2343f7d2222137f55a101d1baa9507257dc231
Abely <abelandk@gmail.com>
2020-05-24 17:21:20 +0200

    Fetch login details from database added

--- springsecurity_01/src/main/java/com/luv2code/springsecurity/config/AppConfig.java
+++ springsecurity_01/src/main/java/com/luv2code/springsecurity/config/AppConfig.java
@@ -1,16 +1,34 @@
 package com.luv2code.springsecurity.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
 public class AppConfig {
+	
+	// Hold the data that is read from the properties file
+	@Autowired
+	private Environment env;
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	//define a bean for View Resolver
 	@Bean
@@ -21,6 +39,45 @@ public class AppConfig {
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
+	}
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create a connection pool
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		
+		// set the jdbc driver class
+		try {
+			dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+		
+		// log the connection props
+		logger.info(">>> jdbc.url " + env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user " + env.getProperty("jdbc.user"));
+		
+		// set the database connection props
+		dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		dataSource.setUser(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+
+		// set connection pool props
+		dataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		dataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		dataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		dataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		return dataSource;
+	}
+	
+	private int getIntProperty(String prop) {
+		
+		String propValue = env.getProperty(prop);
+		
+		int intPropValue = Integer.parseInt(propValue);
+		return intPropValue;
 	}
 
 }
--- springsecurity_01/src/main/java/com/luv2code/springsecurity/config/SecurityConfig.java
+++ springsecurity_01/src/main/java/com/luv2code/springsecurity/config/SecurityConfig.java
@@ -1,31 +1,42 @@
 package com.luv2code.springsecurity.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+/*import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;*/
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired 
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		// Add users for IN MEMORY AUTHENTICATION
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-			.withUser(users.username("abc").password("test1").roles("EMPLOYEE"));
+		/*
+		 * UserBuilder users = User.withDefaultPasswordEncoder();
+		 * auth.inMemoryAuthentication()
+		 * .withUser(users.username("abc").password("test1").roles("EMPLOYEE"));
+		 * 
+		 * auth.inMemoryAuthentication()
+		 * .withUser(users.username("abcd").password("test12").roles("EMPLOYEE",
+		 * "MANAGER"));
+		 * 
+		 * auth.inMemoryAuthentication()
+		 * .withUser(users.username("abcde").password("test123").roles("ADMIN"));
+		 */
 		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("abcd").password("test12").roles("EMPLOYEE", "MANAGER"));
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("abcde").password("test123").roles("ADMIN"));
-		
+		// USE JDBC AUTHENTICATION
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
 	@Override
