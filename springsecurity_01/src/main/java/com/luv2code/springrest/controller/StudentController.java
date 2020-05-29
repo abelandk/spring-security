ab95ed3ccc2a32a5846f4407eb764ecabec3e5e2
Abely <abelandk@gmail.com>
2020-05-29 14:57:20 +0200

    @ControllerAdvice used for Global Exception Handling

--- springsecurity_01/src/main/java/com/luv2code/springrest/controller/StudentController.java
+++ springsecurity_01/src/main/java/com/luv2code/springrest/controller/StudentController.java
@@ -2,13 +2,9 @@ package com.luv2code.springrest.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PrimitiveIterator.OfInt;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,29 +41,5 @@ public class StudentController {
 			throw new StudentNotFoundException("Student Id not found: " + studentId);
 		}
 		return students.get(studentId);
-	}
-	
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(ex.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-		
-	}
-	
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
-		
-		StudentErrorResponse error = new StudentErrorResponse();
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(ex.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-		
 	}
 }
